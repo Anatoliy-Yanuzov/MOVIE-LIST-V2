ea2693207ce227013f4d8e4dc6ba1c53b4fa3462
Anatoliy Yanuzov <anatolii.qnuzov@gmail.com>
2022-05-15 00:15:46 +0300

    created fetch API

--- registerPage.js
+++ registerPage.js
@@ -1,9 +1,41 @@
+import { saveToken } from './auth.js'
+
+let baseUrl = 'http://localhost:3030';
 let registerSection = document.querySelector('.register');
+let registerForm = registerSection.querySelector('#register-form');
+
+registerForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    //let data = new FormData(e.currentTarget)... or
+    let data = new FormData(registerForm);
+
+    fetch(`${baseUrl}/users/register`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: data.get('email'),
+            password: data.get('password')
+        })
+    })
+        .then(res => res.json())
+        .then(responseData => {
+            console.log(responseData);
+            saveToken(responseData.accessToken);
+        });
+})
 
 function showPage() {
     registerSection.classList.remove('hidden');
 }
 
+function hidePage() {
+    registerSection.classList.add('hidden');
+}
+
 export default {
-    showPage
+    showPage,
+    hidePage,
 }
