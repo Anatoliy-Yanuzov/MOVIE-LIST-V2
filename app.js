19d23eef1f5eb396b2c3d30ca29b8a63ee7102cd
Anatoliy Yanuzov <anatolii.qnuzov@gmail.com>
2022-05-16 01:42:30 +0300

    added four imports

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
+import { isAuthenticated } from './auth.js';
 import loginPage from './loginPage.js';
 import registerPage from './registerPage.js';
+import moviesPage from './moviePage.js';
+import navigation from './navigation.js';
+import logout from './logout.js';
 
 let headerElement = document.querySelector('.header .nav');
 
 let pages = {
     register: registerPage,
     login: loginPage,
+    movies: moviesPage,
+    logout,
+}
+navigation.updateNavigation();
+
+if (isAuthenticated()) {
+    moviesPage.showPage();
 }
 headerElement.addEventListener('click', (e) => {
     e.preventDefault();
 
     if (e.target.tagName == 'A') {
         let dataLink = e.target.getAttribute('data-link');
+        navigation.updateNavigation();
 
         if (Object.keys(pages).includes(dataLink)) {
             hidePages();
